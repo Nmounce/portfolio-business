01e95ffdf6bbbe44d8df68abc65b3f1f1c6408b3
Nikki Mounce <nikkimounce@gmail.com>
2021-11-15 12:55:49 -0800

    input items for toolStack

--- src/components/about/tools.js
+++ src/components/about/tools.js
@@ -7,3 +7,28 @@ import {
     SiCodepen,
     SiCanva
 } from 'react-icons/si';
+
+
+function toolStack() {
+    return (
+        <Row style={{}}>
+            <Col xs={4} md={2} className="tech-icons">
+                <SiVisualstudiocode />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                <SiHeroku />
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                < SiInsomnia/>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                < SiCodepen/>
+            </Col>
+            <Col xs={4} md={2} className="tech-icons">
+                < SiCanva/>
+            </Col>
+        </Row>
+    );
+}
+
+export default toolStack;
