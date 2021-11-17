2fd1824c001e0c9b7894628216ca6cfb837da461
Nikki Mounce <nikkimounce@gmail.com>
2021-11-17 12:03:53 -0800

    added starting css files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index";
+import App from "./app.js"
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+    document.getElementById('root')
+);
+
+reportWebVitals();
